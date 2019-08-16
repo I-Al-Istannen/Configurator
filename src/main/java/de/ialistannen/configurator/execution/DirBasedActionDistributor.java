1b40554dd57f7d537cf327af8f068b5e8d18a347
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-08-16 14:27:20 +0200

    Add a run script that contains all actions

--- src/main/java/de/ialistannen/configurator/execution/DirBasedActionDistributor.java
+++ src/main/java/de/ialistannen/configurator/execution/DirBasedActionDistributor.java
@@ -15,8 +15,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -39,7 +42,7 @@ public class DirBasedActionDistributor implements ActionDistributor {
         .orElseThrow(() -> new RuntimeException("Could not find '$" + BASE_DIR_KEY));
     Path baseDir = Paths.get(dir);
     try {
-      distribute(context, baseDir);
+      distribute(generateRunScript(context, baseDir), baseDir);
     } catch (IOException e) {
       throw new DistributionException("Error distributing actions", e);
     }
@@ -59,7 +62,7 @@ public class DirBasedActionDistributor implements ActionDistributor {
     }
 
     for (Action action : context.getAllActions()) {
-      Path actionPath = baseDir.resolve(action.getSanitizedName());
+      Path actionPath = resolveActionPath(baseDir, action);
       if (!dry) {
         Files.write(actionPath, action.getContent().getBytes(StandardCharsets.UTF_8));
         makeExecutable(actionPath);
@@ -75,10 +78,72 @@ public class DirBasedActionDistributor implements ActionDistributor {
     }
   }
 
+  private Path resolveActionPath(Path baseDir, Action action) {
+    return baseDir.resolve(action.getSanitizedName());
+  }
+
   private void makeExecutable(Path path) throws IOException {
     Set<PosixFilePermission> existingPerms = new HashSet<>(Files.getPosixFilePermissions(path));
     existingPerms.add(PosixFilePermission.OWNER_EXECUTE);
 
     Files.setPosixFilePermissions(path, existingPerms);
+  }
+
+  private RenderContext generateRunScript(RenderContext context, Path baseDir) {
+    StringBuilder runAction = new StringBuilder();
+    runAction.append("#/bin/env bash").append(System.lineSeparator())
+        .append("# vim: ft=sh").append(System.lineSeparator());
+
+    List<Action> allActions = context.getAllActions()
+        .stream()
+        .sorted(Comparator.comparing(Action::getName))
+        .collect(Collectors.toList());
+
+    List<String> actionNames = allActions
+        .stream()
+        .map(Action::getName)
+        .sorted()
+        .collect(Collectors.toList());
+    runAction.append(buildRunScriptRofiInvocation(actionNames));
+    runAction.append(System.lineSeparator());
+
+    runAction.append(buildRunScriptCase(allActions, baseDir));
+
+    return context.storeAction(new Action("Run action", runAction.toString()));
+  }
+
+  private String buildRunScriptRofiInvocation(List<String> names) {
+    StringBuilder result = new StringBuilder("CHOICE=$((");
+    String separator = "`";
+    for (String name : names) {
+      result.append("echo -n '").append(name).append(separator).append("' ").append("; ");
+    }
+    result.append(") | rofi -sep '").append(separator).append("' -dmenu)");
+    return result.toString();
+  }
+
+  private String buildRunScriptCase(List<Action> actions, Path baseDir) {
+    StringBuilder result = new StringBuilder()
+        .append("if [ $? -eq 0 ]; then")
+        .append(System.lineSeparator())
+        .append("    case $CHOICE in")
+        .append(System.lineSeparator());
+
+    for (Action action : actions) {
+      result.append("        '")
+          .append(action.getName())
+          .append("')").append(System.lineSeparator())
+          .append("            ").append(resolveActionPath(baseDir, action))
+          .append(System.lineSeparator())
+          .append("            ;;").append(System.lineSeparator());
+    }
+    result.append("    esac").append(System.lineSeparator())
+        .append("    exit").append(System.lineSeparator())
+        .append("else").append(System.lineSeparator())
+        .append("    echo \"Rofi returned failure\"").append(System.lineSeparator())
+        .append("    exit").append(System.lineSeparator())
+        .append("fi")
+        .append(System.lineSeparator());
+    return result.toString();
   }
 }
